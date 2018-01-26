bfc9d68923b8fd23de7dd3dbfe03cf3324888038
z01x02c <z01x02c@users.noreply.github.com>
2018-01-26 18:47:45 +0800

    2016/1/26

---
+++ chapter3/3.3 cars.py
@@ -0,0 +1,16 @@
+cars = ["bmw",'audi','toyota','subaru']
+cars.sort()
+print(cars)
+
+cars.sort(reverse=True)
+print(cars)
+
+cars = ["bmw",'audi','toyota','subaru']
+print(cars)
+print(sorted(cars))
+
+cars = ["bmw",'audi','toyota','subaru']
+cars.reverse()
+print(cars)
+
+print(len(cars))
