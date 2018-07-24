9ce2b43d82b55a0626eb1f5ad57adba0f481480a
z01x02c <33895307+z01x02c@users.noreply.github.com>
2018-07-24 14:24:47 +0800

    2018/6

--- 项目/数据可视化/ch17/python_repos.py
+++ 项目/数据可视化/ch17/python_repos.py
@@ -10,16 +10,37 @@ response_dict = r.json()
 print("Total repositories:", response_dict['total_count'])
 
 repo_dicts = response_dict['items']
+print("Number of items: ", len(repo_dicts))
 
-names, stars = [], []
+names, plot_dicts = [], []
 for repo_dict in repo_dicts:
     names.append(repo_dict['name'])
-    stars.append(repo_dict['stargazers_count'])
+    plot_dict = {
+        'value': repo_dict['stargazers_count'],
+        'label': repo_dict['description'],
+        }
+    plot_dicts.append(plot_dict)
+
+# ~ names, stars = [], []
+# ~ for repo_dict in repo_dicts:
+    # ~ names.append(repo_dict['name'])
+    # ~ stars.append(repo_dict['stargazers_count'])
     
 my_style = LS('#333366', base_style=LCS)
-chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
+
+my_config = pygal.Config()
+my_config.x_label_rotation = 45
+my_config.show_legeng = False
+my_config.title_font_size = 24
+my_config.major_label_font_size = 18
+my_config.truncate_label = 15
+my_config.show_y_guides = False
+my_config.width = 1000
+
+chart = pygal.Bar(my_config, style=my_style)
+# ~ chart = pygal.Bar(style=my_style, x_label_rotation=45, show_legend=False)
 chart.title = "Most-Starred Python Projects on Github"
 chart.x_labels = names
 
-chart.add('', stars)
-chart.render_to_file('python_repos.svg')
+chart.add('', plot_dicts)
+chart.render_to_file('python_repos2.svg')
